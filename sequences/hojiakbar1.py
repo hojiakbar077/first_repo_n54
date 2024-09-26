a3b688b7b6776dfabc62b9940db94ef5015342f4
Hojiakbar <hojiakbar@example.com>
2024-09-27 02:47:53 +0500

    hojiakbar added sequences file and hojiakbar1.py

---
+++ sequences/hojiakbar1.py
@@ -0,0 +1,20 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+print("list :",list)
+
+tuple = (1,2,3,4,5,6,7,8,9,10)
+print("tuple :",tuple)
+
+string = "hojiakbar"
+print("string :",string)
+
+range = range(1,20)
+print("range :",range)
+
+byte = bytes([20,30,10])
+print("byte :",byte)
+
+byte_array = bytearray([20,30,10])
+print("byte_array :",byte_array)
+
+memoryview = memoryview(bytes([20,30,10]))
+print("memoryview :",memoryview)
