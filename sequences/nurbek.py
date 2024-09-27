2977121e257a94e9977ceea958f670449b7b1945
Ismingiz <emailingiz@example.com>
2024-09-27 17:02:40 +0500

    adding new file

---
+++ sequences/nurbek.py
@@ -0,0 +1,21 @@
+string_example = "Hello, World!"
+
+list_example = [1, 2, 3, 4, 5]
+
+tuple_example = (10, 20, 30, 40, 50)
+
+range_example = range(6)
+
+bytes_example = b"Hello"
+
+bytearray_example = bytearray(bytes_example)
+
+memoryview_example = memoryview(bytes_example)
+
+print("String:", string_example)
+print("List:", list_example)
+print("Tuple:", tuple_example)
+print("Range:", list(range_example))
+print("Bytes:", bytes_example)
+print("Bytearray:", bytearray_example)
+print("Memoryview:", list(memoryview_example))
