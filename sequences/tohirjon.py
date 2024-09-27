b3d125d443a4298a9070f8b2e82b6ef3b93b5509
itsmetohirr <itsmetohir@gmail.com>
2024-09-27 14:49:00 +0500

    fixed tohirjon.py

---
+++ sequences/tohirjon.py
@@ -0,0 +1,14 @@
+s_list = [1, 2, 3, "four", True, False]
+
+s_tuple = ("one", "two", "three")
+
+s_range = range(1, 10)
+
+s_string = "This is a sequence."
+
+print(f"""
+list   =>  {s_list}
+tuple  =>  {s_tuple}
+range  =>  {s_range}
+string =>  {s_string}
+""")
