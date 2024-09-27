0a4384e97d994a0daf996c461c08906c7df59c56
rashidiy <bubbless7456@gmail.com>
2024-09-27 20:58:02 +0500

    salom

--- 4.7/iterators/rashidiy.py
+++ 4.7/iterators/rashidiy.py
@@ -1,8 +1,43 @@
-import os
+class Reverse:
+    def __init__(self, s: str):
+        self.s = s
+        self.length = len(s)  # 5
 
-from dotenv import load_dotenv
+    def __iter__(self):
+        return self
 
-load_dotenv()
+    def __next__(self):
+        if not self.length:
+            raise StopIteration
+        self.length -= 1
+        return self.s[self.length]
 
-secret_key = os.getenv('SECRET')
-print(secret_key)
+
+from typing import Union
+
+
+class Cycle:
+    def __iter__(self):
+        return self
+
+    def __init__(self, s: Union[str, list, set, tuple], count: int):
+        self.index = -1
+        self.s = s
+        self.count = count
+        self.length = len(s)  # noqa
+
+    def __next__(self):
+        self.index += 1
+        if self.index == self.length:
+            self.index = 0
+            self.count -= 1
+        if not self.count:
+            raise StopIteration
+        return self.s[self.index]  # noqa
+
+
+def men_f():
+    n = 0
+    while n < 100:
+        yield n
+        n += 3
